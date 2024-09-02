d8e950c0a92333e78981677adbb0a41142949a35
Bernardo <bernardo.abreufig@outlook.com>
2024-09-02 14:50:37 +0200

    used new approach with nonlinear constraints

--- convexopt_xsuite_simple.py
+++ convexopt_xsuite_simple.py
@@ -23,6 +23,14 @@ opt = line.match(
         xt.TargetSet(dqx=10.0, dqy=12.0, tol=0.01, tag='chrom'),
     ])
 
+def twiss_nonlinear_up():
+    aa = opt.targets[0].action
+    qx = merit_function.merit_function._last_data[aa].qx - opt.targets[0].value - 1e-6
+    qy = merit_function.merit_function._last_data[aa].qy - opt.targets[1].value - 1e-6
+    dqx = merit_function.merit_function._last_data[aa].dqx - opt.targets[2].value - 0.01
+    dqy = merit_function.merit_function._last_data[aa].dqy - opt.targets[3].value - 0.01
+
+    return np.array([qx, qy, dqx, dqy])
 
 merit_function = opt.get_merit_function(return_scalar=False, check_limits=False)
 
@@ -30,14 +38,6 @@ bounds = merit_function.get_x_limits()
 x0 = merit_function.get_x()
 
 # To satisfy Gx = h as the constraints
-# G = np.zeros((len(bounds) * 2,len(bounds)), dtype=float)
-# for i in range(0, len(G), 2):
-#     if i > 1:
-#         G[i][i//2] = -1.0
-#         G[i+1][i//2] = 1.0
-# G = matrix(G)
-# h = matrix(bounds.flatten())
-
 G = np.zeros((len(bounds),len(bounds)), dtype=float)
 for i in range(0, len(G), 2):
     G[i][i//2 + 2] = -1.0
@@ -49,22 +49,26 @@ rows = []
 
 def F(x=None, z=None):
     if x is None:
-        return 0, matrix(merit_function.get_x())
+        return 4, matrix(merit_function.get_x())
 
     f = merit_function(x)
+    constr = twiss_nonlinear_up()
+
     jacobian = merit_function.merit_function.get_jacobian(x)
 
-    f = np.sum(f)
+    f = np.concat(([np.sum(f)], constr))
     f_jacobian = np.sum(jacobian, axis=0).reshape(1, len(jacobian[0]))
+    f_jacobian = np.concat((f_jacobian, jacobian))
+    
 
     if z is None:
         return matrix(f), matrix(f_jacobian)
     # Compute the Lagrangian Hessian, ensuring it's an n x n matrix
-    H = z[0] * (f_jacobian.T @ f_jacobian)
+    H = z[0] * (f_jacobian.T @ f_jacobian) + z[1:5] * (jacobian.T @ jacobian)
+    opt.target_status()
     return matrix(f), matrix(f_jacobian), matrix(H)
 
 # Solve
-# Throws error because rank of H = 1 (smaller than n = 4)
 solution = solvers.cp(F, G=G, h=h)
 
 # Optimal solution
