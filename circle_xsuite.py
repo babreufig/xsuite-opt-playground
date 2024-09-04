8d9cead6529308a790195703fbbda669e57cee04
Bernardo Abreu Figueiredo <bernardo.abreu.figueiredo@cern.ch>
2024-09-04 11:17:52 +0200

    added inequality constraints and weighted targets to xsuite circle exampler

--- circle_xsuite.py
+++ circle_xsuite.py
@@ -3,21 +3,34 @@ import xtrack as xt
 import numpy as np
 
 def f(x):
-    return [x[0], x[0]**2 + x[1]**2 - 4]
+    return [x[0], x[0]**2 + x[1]**2, x[0]**2 + x[1]**2]
 
-x0 = [-0.5,1.2]
+x0 = [1.0,0.0]
 steps = [1e-8, 1e-8]
-tar = [-1., 0.]
-tols = [1e-6, 1e-6, 1e-6]
+tar = [-2., xt.match.LessThan(4.0), xt.match.GreaterThan(3.9)]
+tols = [1e-8, 1e-8, 1e-8]
 
 vary = [xt.Vary(ii, container=x0, step=steps[ii]) for ii in range(len(x0))]
 
 
 opt = opti.Optimize(vary=vary, targets=xt.match.ActionCall(f, vary).get_targets(tar))
+opt.targets[0].weight = 2
 
 for ii, tt in enumerate(opt.targets):
         tt.tol = tols[ii]
 
-opt.solve()
+it = 0
+for i in range(opt.n_steps_max):
+    old_x = opt.solver.x if opt.solver.x is not None else x0.copy()
+    if opt.solver.stopped is not None:
+        break
+    it += 1
+    opt.solver.x = opt._err._knobs_to_x(opt._extract_knob_values())
+    opt.step(1)
+    opt.set_knobs_from_x(opt.solver.x)
+    knobs = opt._extract_knob_values()
+
+    print(f"Iteration {it}: ({np.round(old_x[0], 3)}, {np.round(old_x[1], 3)}) " + \
+          f"---> ({np.round(knobs[0], 3)}, {np.round(knobs[1], 3)})")
+
 opt.target_status()
-print(opt.get_knob_values())
