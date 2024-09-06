54c801e194448ff129270901b947d586b8dbd414
Bernardo Abreu Figueiredo <bernardo.abreu.figueiredo@cern.ch>
2024-09-06 11:15:33 +0200

    added circle example with pygmo

---
+++ circle_pygmo.py
@@ -0,0 +1,37 @@
+import pygmo as pg
+import numpy as np
+
+class Circle_Problem:
+
+    def fitness(self, x):
+        obj = x[0]
+        ci1 = -x[0]**2 - x[1]**2 + 3
+        ci2 = x[0]**2 + x[1]**2 - 4
+
+        return [x[0], ci1, ci2]
+    
+    def get_nic(self):
+        return 2 # Number of inequality constraints
+    
+    def get_nec(self):
+        return 0 # Number of equality constraints
+    
+    def get_bounds(self):
+        return ([-8]*2, [8]*2)
+    
+    def get_nobj(self):
+        return 1 # Number of objectives
+    
+    def gradient(self, x):
+        return ([1, 0, -2*x[0], -2*x[1], 2*x[0], 2*x[1]])
+
+udp = Circle_Problem()
+prob = pg.problem(udp)
+algo = pg.algorithm(uda = pg.nlopt('auglag'))
+algo.extract(pg.nlopt).local_optimizer = pg.nlopt('var2')
+algo.set_verbosity(200)
+pop = pg.population(prob = udp, size = 1)
+pop = algo.evolve(pop)
+
+print(pop.champion_f)
+print(pop.champion_x) 
--- convexopt_multi.py
+++ convexopt_multi.py
@@ -1,8 +1,8 @@
 from cvxopt import matrix, solvers
 
 # Multivariate example for cvxopt.cp:
-# Minimize functions f0(x,y) = (x-1)² + (y-2)² ; f1(x,y) = (x + y - 3)² ; f2(x, y) = (x - y)² (two variables, one target)
-# under constraints:
+# Minimize functions f0(x,y) = (x-1)² + (y-2)² ; f1(x,y) = (x + y - 3)² ; f2(x, y) = (x - y)²
+# (two variables, one target) under constraints:
 # 1. x + y >= 3
 # 2. x >= 0
 # 3. y >= 0
@@ -12,21 +12,22 @@ def F(x=None, z=None):
     if x is None:  # Initial guess (2.2, 1.2), zero nonlinear functions
         return 0, matrix([2.2, 1.2])
     
-    # Zielfunktion
+    # Objective function
     f = matrix([(x[0] - 1)**2 + (x[1] - 2)**2 +  # f1(x, y)
                 (x[0] + x[1] - 3)**2 +            # f2(x, y)
                 (x[0] - x[1])**2],               # f3(x, y)
                (1, 1))
     
     # Gradient
-    Df = matrix([2 * (x[0] - 1) + 2 * (x[0] + x[1] - 3) + 2 * (x[0] - x[1]),  # Gradient of f1
-                 2 * (x[1] - 2) + 2 * (x[0] + x[1] - 3) + -2 * (x[0] - x[1]),  # Gradient of f2
-                 ]).T  # Gradient of f3
+    Df = matrix([2 * (x[0] - 1) + 2 * (x[0] + x[1] - 3) + 2 * (x[0] - x[1]),
+                 2 * (x[1] - 2) + 2 * (x[0] + x[1] - 3) -2 * (x[0] - x[1])]).T
     
     if z is None:
         return f, Df
-    
-    H = z[0] * (matrix([[2.0, 0.0], [0.0, 2.0]]) + matrix([[2.0, 2.0], [2.0, 2.0]]) + matrix([[2.0, -2.0], [-2.0, 2.0]]))
+    # Hessian
+    H = z[0] * (matrix([[2.0, 0.0], [0.0, 2.0]])
+                + matrix([[2.0, 2.0], [2.0, 2.0]])
+                + matrix([[2.0, -2.0], [-2.0, 2.0]]))
     return f, Df, H
 
 # ## Does not work. For a vector function, needs to sum all functions, as well as Jacobians and Hessians
@@ -55,7 +56,7 @@ def F(x=None, z=None):
 
 # Inequality Constraints:
 G = matrix([[-1.0, -1.0], [-1.0, 0.0], [0.0, -1.0]]).T
-h = matrix([3.0, 0.0, 0.0])
+h = matrix([-3.0, 0.0, 0.0])
 
 ## Equality Constraints
 
