2bf000758f426b1cd43da9ca6ca193e390379b32
Bernardo Abreu Figueiredo <bernardo.abreufig@outlook.com>
2025-02-18 15:59:55 +0100

    validate ddy

--- betabeating/r_matrix_deriv.py
+++ betabeating/r_matrix_deriv.py
@@ -14,7 +14,7 @@ env['bphi'] = 0.
 
 line = env.new_line(components=[
     env.new('qf', 'Quadrupole', k1='kq', length=0.5, anchor='start', at=1.),
-    env.new('bend', 'Bend', angle='bphi', k0='bphi', at=0., anchor='start', length=1.0),
+    env.new('bend', 'Bend', angle='bphi', k0='bphi', rot_s_rad=np.pi/2, at=0., anchor='start', length=1.0),
     env.new('qd', 'Quadrupole', k1='-kq', length=0.5, anchor='start', at=11.,
             from_='qf@end'),
     env.new('end', 'Marker', at=10., from_='qd@end'),
@@ -73,8 +73,8 @@ dx_arr = np.zeros(len(dk1l_arr))
 for i, dk1l in enumerate(dk1l_arr):
     env['dk1l'] = dk1l
     twnew = line.twiss(init=init_sec)
-    dx_arr[i] = twnew.dx[-1]
-    analytic_ddx = twnew.get_twiss_param_derivative('end', 'dquad')['ddx']
+    dx_arr[i] = twnew.dpy[-1]
+    analytic_ddx = twnew.get_twiss_param_derivative('end', 'dquad')['ddpy']
 
 gradient = (dx_arr[1:] - dx_arr[:-1])/(dk1l_arr[1] - dk1l_arr[0])
 
