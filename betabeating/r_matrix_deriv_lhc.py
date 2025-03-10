12a3c2dac2e1ede5626f03f6bb7f282e5ce36b33
Bernardo Abreu Figueiredo <bernardo.abreufig@outlook.com>
2025-03-10 09:22:42 +0100

    Starting script to validate derivatives for lhc lattice

--- betabeating/r_matrix_deriv_lhc.py
+++ betabeating/r_matrix_deriv_lhc.py
@@ -1,20 +1,20 @@
-# Optics calculation and matching for a large ring (LHC) - part 1
-
 import xtrack as xt
-import lhc_match as lm
+import varlimits_helper as vlm
 
 import numpy as np
 import os
-import pandas as pd
-import json
+
+env = xt.Environment()
+env.particle_ref = xt.Particles(p0c=7e12)
+env['dk1l'] = 0.
 
 # Load LHC model
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 print(dir_path)
 
-collider = xt.Multiline.from_json(dir_path + "/hllhc15_collider_thick.json")
-collider.vars.load_madx(dir_path + "/opt_round_150_1500.madx")
+collider = env.from_json(dir_path + "/../hllhc15_collider_thick.json")
+collider.vars.load_madx(dir_path + "/../opt_round_150_1500.madx")
 
 collider.build_trackers()
 
@@ -26,7 +26,7 @@ tw0.rows['ip.*'].cols['betx bety mux muy x y']
 
 
 # Prepare for optics matching: set limits and steps for all circuits
-lm.set_var_limits_and_steps(collider)
+vlm.set_var_limits_and_steps(collider)
 
 # Inspect for one circuit
 collider.vars.vary_default['kq4.l2b2']
@@ -35,6 +35,9 @@ collider.vars.vary_default['kq4.l2b2']
 tw_81_12 = collider.lhcb1.twiss(start='ip8', end='ip2', init_at='ip1',
                                 betx=0.15, bety=0.15)
 
+# add multipole at IP8 for calculating derivatives
+
+extra_line = env.new_line(components=[env.new('dquad', 'Multipole', knl=[0., 'dk1l'], at='ip8')])
 
 # %%
 opt = collider.lhcb1.match(
@@ -55,31 +58,3 @@ opt = collider.lhcb1.match(
         xt.TargetRelPhaseAdvance('mux', value = tw0['mux', 'ip1.l1'] - tw0['mux', 's.ds.l8.b1']),
         xt.TargetRelPhaseAdvance('muy', value = tw0['muy', 'ip1.l1'] - tw0['muy', 's.ds.l8.b1']),
     ])
-
-opt.check_limits = False
-
-opt.target_status()
-
-knob_values_old = np.array(list(opt.get_knob_values().values())) # Get old knob values
-
-print("---------------------------------------------------------------------")
-print(f"Solving for bety: 0.15 --> 0.1")
-
-opt._err.call_counter = 0
-opt.solve(broyden=True, n_steps=60)
-#opt.step(1, broyden=False)
-#opt.step(30, broyden=True)
-# opt.step(10, broyden=True)
-# #opt.step(2, broyden=False)
-# #opt.step(5, broyden=True)
-
-
-n_calls = opt._err.call_counter
-
-#x_sol = opt._err._get_x()
-
-opt.target_status()
-# knob_values_new = opt._err._x_to_knobs(x_sol)
-# knob_values_diff = knob_values_new - knob_values_old
-
-print(opt.log())
